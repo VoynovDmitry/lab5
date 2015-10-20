e1759abeb8f2dadca4d826d990de011dd3d83de6
VoynovDmitry <cry_miss@mail.ru>
2015-10-20 22:52:00 +0300

    task 2 added tests

--- laba5_2.cpp
+++ laba5_2.cpp
@@ -49,8 +49,20 @@ char maxmin(char *str)
 	return head->ch;
 }
 
+void assert(char test[], char ans)
+{
+	if (maxmin(test) == ans) cout << "OK" << endl;
+	else cout << "FAIL" << endl;
+}
+
 int main()
 {
+	assert("max(4,min(max(1,3),min(8,5)))", '4');
+	assert("max(1,7)", '7');
+	assert("min(max(min(2,5),max(3,4)),min(max(8,min(9,9)),8))", '4');
+	assert("max(1,min(2,max(3,min(4,max(5,min(6,max(7,min(8,9))))))))", '2');
+
+	
 	
 	return 0;
 }
